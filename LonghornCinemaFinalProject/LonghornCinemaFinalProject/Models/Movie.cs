b56a4bae036f3c9afd3e53c17a454438b6524c37
SamuelGutierrezBBAMIS <35734558+SamuelGutierrezBBAMIS@users.noreply.github.com>
2018-03-25 19:57:47 -0500

    Test comment

--- LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/Movie.cs
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/Movie.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+// extra comment
+
 namespace LonghornCinemaFinalProject.Models
 {
     public enum MovieGenre { Horror, Drama, Comedy}
