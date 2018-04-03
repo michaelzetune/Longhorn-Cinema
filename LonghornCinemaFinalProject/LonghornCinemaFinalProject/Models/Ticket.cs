417516f7fbc25a638ebc8625d8dc56e80b2659ef
MichaelZetune <michael.zetune@utexas.edu>
2018-04-02 22:57:46 -0500

    Add Ticket Nav Properties

--- LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/Ticket.cs
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/Ticket.cs
@@ -10,13 +10,20 @@ namespace LonghornCinemaFinalProject.Models
         // Properties
         // TicketID
         public Int32 TicketID { get; set; }
+        // Seat
+        public String Seat { get; set; }
+
+        /* Commenting this out because I think TicketPrice and MoviePrice would be the same value (-Michael) :
         // Price
-        public Decimal TicketPrice { get; set; }
+        public Decimal TicketPrice { get; set; }        */
 
         // Navigation Properties
         // Order
+        public virtual Order Order { get; set; }
         // Showing
+        public virtual Showing Showing { get; set; }
         // MoviePrice
+        public virtual MoviePrice MoviePrice { get; set; }
 
     }
 }
