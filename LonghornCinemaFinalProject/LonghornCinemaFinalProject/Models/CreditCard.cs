e847ff0c2c8ff7e5854e276a783a0a358365a146
MichaelZetune <michael.zetune@utexas.edu>
2018-04-10 01:05:24 -0500

    Comment out errors for now, fix later on

--- LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/CreditCard.cs
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/CreditCard.cs
@@ -20,32 +20,32 @@ namespace LonghornCinemaFinalProject.Models
         public String CardNumber { get; set; }
 
         // CardType
-        public CardType CardType
-        {
-            get { return CardType; }
-            set {
-                    switch (CardNumber)
-                    {
-                        case CardNumber.Length == 15:
-                            CardType = CardType.Amex; break;
+        //public CardType CardType
+        //{
+        //    get { return CardType; }
+        //    set {
+        //            switch (CardNumber)
+        //            {
+        //                case CardNumber.Length == 15:
+        //                    CardType = CardType.Amex; break;
 
-                        case CardNumber.Substring(0,2).Equals("54"): // this is a Mastercard
-                            CardType = CardType.MasterCard; break;
+        //                case CardNumber.Substring(0,2).Equals("54"): // this is a Mastercard
+        //                    CardType = CardType.MasterCard; break;
 
-                        case CardNumber.Substring(0, 1).Equals("4"): // number is a Visa card
-                            CardType = CardType.Visa; break;
+        //                case CardNumber.Substring(0, 1).Equals("4"): // number is a Visa card
+        //                    CardType = CardType.Visa; break;
 
-                        case CardNumber.Substring(0, 1).Equals("6"): // number is a Discover card
-                            CardType = CardType.Discover; break;
+        //                case CardNumber.Substring(0, 1).Equals("6"): // number is a Discover card
+        //                    CardType = CardType.Discover; break;
 
-                        default: // if the card type doesn't fulfill one of the conditions above, the user messed up
-                            Console.WriteLine("Card type not valid");
-                            CardType = null; break;
-                    }
+        //                default: // if the card type doesn't fulfill one of the conditions above, the user messed up
+        //                    Console.WriteLine("Card type not valid");
+        //                    CardType = null; break;
+        //            }
 
 
-                }
-        }
+        //        }
+        //}
 
 
         // Navigation Properties
