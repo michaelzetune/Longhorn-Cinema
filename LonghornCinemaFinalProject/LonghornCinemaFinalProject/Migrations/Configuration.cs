c14e165b9ee0b386f7eb77f5343aa22909c52e9f
MichaelZetune <michael.zetune@utexas.edu>
2018-04-10 01:05:01 -0500

    Update Configuration to seed Movies with MovieData
    This currently throws an error with update-database

---
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Migrations/Configuration.cs
@@ -0,0 +1,27 @@
+namespace LonghornCinemaFinalProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LonghornCinemaFinalProject.DAL.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LonghornCinemaFinalProject.DAL.AppDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+
+            MovieData AddMovies = new MovieData();
+            AddMovies.SeedMovies(context);
+
+        }
+    }
+}
