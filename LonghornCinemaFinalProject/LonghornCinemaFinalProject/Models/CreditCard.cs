92ea0ffc4d1e05ac8b956b44a0b2a18ba7d32c22
MichaelZetune <michael.zetune@utexas.edu>
2018-04-23 00:38:46 -0500

    Fix CreditCard.cs

--- LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/CreditCard.cs
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/CreditCard.cs
@@ -11,41 +11,28 @@ namespace LonghornCinemaFinalProject.Models
 
     public class CreditCard
     {
-        // Properties
-        // CreditCardID
         public Int32 CreditCardID { get; set; }
 
-        // CardNumber
-        [StringLength(15)] // this needs to be 15-16 somehow
+        [StringLength(16, MinimumLength=15)]
         public String CardNumber { get; set; }
 
         // CardType
-        //public CardType CardType
-        //{
-        //    get { return CardType; }
-        //    set {
-        //            switch (CardNumber)
-        //            {
-        //                case CardNumber.Length == 15:
-        //                    CardType = CardType.Amex; break;
-
-        //                case CardNumber.Substring(0,2).Equals("54"): // this is a Mastercard
-        //                    CardType = CardType.MasterCard; break;
-
-        //                case CardNumber.Substring(0, 1).Equals("4"): // number is a Visa card
-        //                    CardType = CardType.Visa; break;
-
-        //                case CardNumber.Substring(0, 1).Equals("6"): // number is a Discover card
-        //                    CardType = CardType.Discover; break;
-
-        //                default: // if the card type doesn't fulfill one of the conditions above, the user messed up
-        //                    Console.WriteLine("Card type not valid");
-        //                    CardType = null; break;
-        //            }
-
-
-        //        }
-        //}
+        public CardType CardType
+        {
+            get
+            {
+                if (CardNumber.Length == 15)
+                    return CardType.Amex;
+                else if (CardNumber.Substring(0, 2) == "54")
+                    return CardType.MasterCard;
+                else if (CardNumber.Substring(0, 1) == "4")
+                    return CardType.Visa;
+                else if (CardNumber.Substring(0, 1) == "6")
+                    return CardType.Discover;
+                else
+                    throw new System.Exception("Card Number is not in a valid format");
+            }
+        }
 
 
         // Navigation Properties
