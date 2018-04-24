8f04f55c2c350b44ab6f5de5b5f0f6fac10f28df
MichaelZetune <michael.zetune@utexas.edu>
2018-04-24 17:49:59 -0500

    Add Email Capability

---
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Utilities/EmailMessaging.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Net.Mail;
+using System.Net;
+
+namespace LonghornCinemaFinalProject.Utilities
+{
+    public class EmailMessaging
+    {
+
+        public static void SendEmail(String toEmailAddress, String emailSubject, String emailBody)
+        {
+            //Create an email client to send the emails     
+            var client = new SmtpClient("smtp.gmail.com", 587) {Credentials = new NetworkCredential("LonghornCinema333@gmail.com", "team5FTW!"),EnableSsl = true};
+
+            //Add anything that you need to the body of the message     
+            // /n is a new line – this will add some white space after the main body of the message            
+            
+            //Create an email address object for the sender address     
+            MailAddress senderEmail = new MailAddress("LonghornCinema333@gmail.com", "Team 5");
+            MailMessage mm = new MailMessage();
+            mm.Subject = "Team 5 - " + emailSubject;
+            mm.Sender = senderEmail;
+            mm.From = senderEmail;
+            mm.To.Add(new MailAddress(toEmailAddress));
+            mm.Body = emailBody;
+            client.Send(mm);
+        }
+
+    }
+}
