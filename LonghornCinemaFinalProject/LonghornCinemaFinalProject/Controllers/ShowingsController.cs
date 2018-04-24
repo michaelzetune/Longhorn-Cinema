1c5a271f8ef57e46680008272f5f4b71c510abdc
Ben Hurbrough <mybroben@users.noreply.github.com>
2018-04-24 14:12:10 -0500

    Adds Showing Controller & Views
    Updates showing model class, adds views for showing and adds a controller for it

---
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Controllers/ShowingsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LonghornCinemaFinalProject.DAL;
+using LonghornCinemaFinalProject.Models;
+
+namespace LonghornCinemaFinalProject.Controllers
+{
+    public class ShowingsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Showings
+        public ActionResult Index()
+        {
+            return View(db.Showings.ToList());
+        }
+
+        // GET: Showings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Showing showing = db.Showings.Find(id);
+            if (showing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showing);
+        }
+
+        // GET: Showings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Showings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShowingID,StartTime,EndTime,SpecialEvent,TheatreNum,SeatList")] Showing showing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Showings.Add(showing);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(showing);
+        }
+
+        // GET: Showings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Showing showing = db.Showings.Find(id);
+            if (showing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showing);
+        }
+
+        // POST: Showings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShowingID,StartTime,EndTime,SpecialEvent,TheatreNum,SeatList")] Showing showing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(showing).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(showing);
+        }
+
+        // GET: Showings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Showing showing = db.Showings.Find(id);
+            if (showing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showing);
+        }
+
+        // POST: Showings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Showing showing = db.Showings.Find(id);
+            db.Showings.Remove(showing);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
