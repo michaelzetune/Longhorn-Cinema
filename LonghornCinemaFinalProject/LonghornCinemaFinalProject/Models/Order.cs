05fabdbf5b3d843e64f469fcd027fbb9b6189e15
MichaelZetune <michael.zetune@utexas.edu>
2018-04-26 11:26:18 -0500

    Delete ConfirmationID from Order

--- LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/Order.cs
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/Order.cs
@@ -12,9 +12,6 @@ namespace LonghornCinemaFinalProject.Models
         // OrderID
         public Int32 OrderID { get; set; }
 
-        //TODO: implement get for transaction ID/add utility class
-        public Int32 ConfirmationID { get; set; }
-
         public Boolean Complete { get; set; }
 
         // Navigation Properties
