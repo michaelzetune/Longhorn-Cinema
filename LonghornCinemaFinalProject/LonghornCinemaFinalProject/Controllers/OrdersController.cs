2e66758135a9d2a90e7f41963b9acef2fb2a8be3
Ben Hurbrough <mybroben@users.noreply.github.com>
2018-04-27 15:02:46 -0500

    Unfinished Order Process.cs

--- LonghornCinemaFinalProject/LonghornCinemaFinalProject/Controllers/OrdersController.cs
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Controllers/OrdersController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using LonghornCinemaFinalProject.DAL;
 using LonghornCinemaFinalProject.Models;
+using Microsoft.AspNet.Identity;
 
 namespace LonghornCinemaFinalProject.Controllers
 {
@@ -37,20 +38,10 @@ namespace LonghornCinemaFinalProject.Controllers
         }
 
         // GET: Orders/Create
-        public ActionResult Create(int OrderID, int ShowingID)
+        public ActionResult Create(int ShowingID)
         {
-
-            Showing show = db.Showings.Find(ShowingID);
-           
-            Ticket tic = new Ticket();
-
-            Order ord = db.Orders.Find(OrderID);
-
-            tic.Order = ord;
-
-            tic.Showing = show;
-
-            return View(tic);
+            Showing showing = db.Showings.Find(ShowingID);
+            return View();
         }
 
         // POST: Orders/Create
@@ -58,68 +49,26 @@ namespace LonghornCinemaFinalProject.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "OrderID")] Order order, Ticket tic, int SelectedShowing, int SelectedMoviePrice, int UserID)
+        public ActionResult Create([Bind(Include = "OrderID")] Order order, int SelectedShowingID, int UserID)
         {
-            // find next order number
-            //AppUser user = db.Users.Find(User.Identity.GetUserId());
-            //order.TicketPrice = Utilities.GenerateTicketPrice.GetNextOrderNumber();
 
             //Record date of order
             order.OrderDate = DateTime.Today;
+            order.Complete = false;
 
-            Showing showing = db.Showings.Find(SelectedShowing);
+            Showing showing = db.Showings.Find(SelectedShowingID);
 
-            MoviePrice movieprice = db.MoviePrices.Find(SelectedMoviePrice);
-            tic.Showing = showing;
-
-            Order ord = db.Orders.Find(tic.Order.OrderID);
-
-            AppUser user = db.Users.Find(UserID);
-
-            tic.Order = ord;
-
-            tic.TicketPrice = Utilities.GenerateTicketPrice.GetTicketPrice(showing.StartTime);
-
-            if (showing.SpecialEventStatus == SpecialEvent.NotSpecial)
-            {
-                //sets senior citizen discount
-                //TODO: Add logic for only allowing discount for 2 tickets per transaction
-                if ((DateTime.Now.Year - user.Birthday.Year) >= 60)
-                {
-                    tic.TicketPrice = tic.TicketPrice - 2;
-                    ViewBag.SeniorCitizen = "$2 Senior Citizen Discount Applied";
-                }
-                else
-                {
-                    ViewBag.SeniorCitizen = "No Discount";
-                }
-
-                //vars for determining time between now and showing start time
-                Int32 intDayHours = (DateTime.Now.Day - showing.StartTime.Day) * 24;
-                Int32 intHours = (DateTime.Now.Hour - showing.StartTime.Hour);
-                Int32 intTotalHours = intDayHours + intHours;
-
-                //sets discount if ticket is purchased 48 hours in advance
-                if ((intTotalHours >= 48))
-                {
-                    tic.TicketPrice = tic.TicketPrice - 1;
-                    ViewBag.Advance = "$1 Discount for Purchasing Early";
-                }
-                else
-                {
-                    ViewBag.Advance = "No Discount";
-                }
-            }
-            tic.ExtendedPrice = tic.TicketPrice * tic.Quantity;
+            AppUser user = db.Users.Find(User.Identity.GetUserId());
+            order.AppUser = user;
 
             if (ModelState.IsValid)
             {
-                db.Tickets.Add(tic);
+                db.Orders.Add(order);
                 db.SaveChanges();
-                return RedirectToAction("Details", "Orders", new { id = ord.OrderID });
+                return RedirectToAction("Create", "Tickets", new { ordid = order.OrderID, showid = showing.ShowingID });
             }
 
-            return View(tic);
+            return View(order);
         }
 
         // GET: Orders/Edit/5
