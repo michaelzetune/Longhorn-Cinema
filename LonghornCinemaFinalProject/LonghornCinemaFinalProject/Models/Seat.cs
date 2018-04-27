a1e9a1d6f78fc460b43925ace449460db45418b1
Ben Hurbrough <mybroben@users.noreply.github.com>
2018-04-27 13:47:36 -0500

    stuff

---
+++ LonghornCinemaFinalProject/LonghornCinemaFinalProject/Models/Seat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LonghornCinemaFinalProject.Models
+{
+    public class Seat
+    {
+        public Int32 SeatID { get; set; }
+
+        public String SeatName { get; set; }
+    }
+}
